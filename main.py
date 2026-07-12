2e18a96723559602fe55988cc6b88f3db481eb79
sinyakumari6-cell <sinyakumari6@gmail.com>
2026-07-12 13:03:34 +0500

    Feat: FastAPI local server setup with PostgreSQL connection check

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from fastapi import FastAPI
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+# .env file load karne ke liye
+load_dotenv()
 
 app = FastAPI()
 
-@app.get("/data")
-def read_data():
-    return {"message": "Yeh mera pehla endpoint hai!"}
+def check_db_connection():
+    try:
+        connection = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            port=os.getenv("DB_PORT")
+        )
+        connection.close()
+        return "Connected Successfully! 🚀"
+    except Exception as e:
+        return f"Connection Failed: {str(e)} ❌"
 
 @app.get("/status")
 def read_status():
-    return {"status": "success", "intern": "doing-great"}
+    db_status = check_db_connection()
+    return {
+        "status": "success", 
+        "intern": "doing-great",
+        "database_status": db_status
+    }
