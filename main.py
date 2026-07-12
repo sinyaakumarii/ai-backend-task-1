b9afc7e3ff4032796fb9956b60be1cdc99f2e39e
sinyakumari6-cell <sinyakumari6@gmail.com>
2026-07-12 13:12:22 +0500

    Feat: Integrate SQLAlchemy models and database connection with FastAPI

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
-from fastapi import FastAPI
-import os
-import psycopg2
-from dotenv import load_dotenv
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+import models
+from database import engine, get_db
 
-# .env file load karne ke liye
-load_dotenv()
-
+# Ye line database me saare tables automatic create kar degi agar wo nahi bane hain
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
-
-def check_db_connection():
-    try:
-        connection = psycopg2.connect(
-            host=os.getenv("DB_HOST"),
-            database=os.getenv("DB_NAME"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            port=os.getenv("DB_PORT")
-        )
-        connection.close()
-        return "Connected Successfully! 🚀"
-    except Exception as e:
-        return f"Connection Failed: {str(e)} ❌"
 
 @app.get("/status")
 def read_status():
-    db_status = check_db_connection()
-    return {
-        "status": "success", 
-        "intern": "doing-great",
-        "database_status": db_status
-    }
+    return {"status": "success", "database": "Connected & Tables Created! 🚀"}
+
+# Ek test route jo database me naya task add karega
+@app.post("/tasks/create")
+def create_task(title: str, description: str = None, db: Session = Depends(get_db)):
+    new_task = models.Task(title=title, description=description)
+    db.add(new_task)
+    db.commit()
+    db.refresh(new_task)
+    return {"message": "Task created successfully!", "task": new_task}
